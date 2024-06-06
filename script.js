a8c8ce26610826fdf99c129eccf6b3624ed37934
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2024-06-06 11:32:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let images = [
+let images = [  // images that will appear in the html
   "dice-01.svg",
   "dice-02.svg",
   "dice-03.svg",
@@ -11,10 +11,10 @@ let currentPlayer = 1;
 function roll() {
   dice.forEach((die) => die.classList.add("shake"));
   setTimeout(() => {
-    dice.forEach((die) => die.classList.remove("shake"));
+    dice.forEach((die) => die.classList.remove("shake")); // this part will make the images shake from css
 
     let dieOneValue = Math.floor(Math.random() * 6) + 1; // Dice values should be 1-6
-    let dieTwoValue = Math.floor(Math.random() * 6) + 1;
+    let dieTwoValue = Math.floor(Math.random() * 6) + 1; 
     let total = dieOneValue + dieTwoValue;
 
     console.log(dieOneValue, dieTwoValue);
@@ -24,23 +24,23 @@ function roll() {
       .setAttribute("src", images[dieOneValue - 1]);
     document
       .querySelector("#die-2")
-      .setAttribute("src", images[dieTwoValue - 1]);
+      .setAttribute("src", images[dieTwoValue - 1]);  // this code will change the picture from the html
 
-    // game rules
+    // game rules code showed below
 
     let message = `Player ${currentPlayer}`;
     if (total === 7) {
-      message += " 🎖🎉🎉wins!";
+      message += " 🎉🎉🎖wins!";
      
-    } else if (total === 9 || total === 3) {
+    } else if (total === 9 || total === 3) {  // we make two total give the same results
       message += " 😥loses!";
     }else if( total === 12){
-      message += '\n' + '+1 point💥 added to your current SCORE!'
+      message += '\n' + '+3 point💥 added to your current SCORE!'
     }
     
     else {
       message += "'s  turn";
-      currentPlayer = currentPlayer === 1 ? 2 : 1;
+      currentPlayer = currentPlayer === 1 ? 2 : 1;  
     }
 
     document.querySelector("#total").innerHTML = message;
